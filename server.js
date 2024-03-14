72ff3c63d3b0e620e70efee8932961bf7325dba6
desainiravp <125334778+desainiravp@users.noreply.github.com>
2024-03-14 12:19:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const promClient = require('prom-client');
 
@@ -23,27 +22,12 @@ app.get('/', (req, res) => {
     counter.inc();
 
     // Send a response
-=======
-const http = require('http');
-
-const hostname = '0.0.0.0';
-const port = 9091;
-
-const server = http.createServer((req, res) => {
->>>>>>> f70ba8e58261b886aa10e95811a239a573405941
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello from port 9091!\n');
 });
 
-<<<<<<< HEAD
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
-
-=======
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
->>>>>>> f70ba8e58261b886aa10e95811a239a573405941
